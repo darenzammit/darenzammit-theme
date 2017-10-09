6643a7781957f9d339bd554a9ac8b84c61ed67de
Daren Zammit <daren@hive.com.mt>
2017-10-09 09:04:47 +0200

    Removed primary div

--- 404.php
+++ 404.php
@@ -9,26 +9,24 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+	<main id="main" class="site-main">
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'darenzammit' ); ?></h1>
-				</header><!-- .page-header -->
+		<section class="error-404 not-found">
+			<header class="page-header">
+				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'darenzammit' ); ?></h1>
+			</header><!-- .page-header -->
 
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'darenzammit' ); ?></p>
+			<div class="page-content">
+				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'darenzammit' ); ?></p>
 
-					<?php get_search_form();?>
-					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
-					<?php the_widget( 'WP_Widget_Archives' ); ?>
+				<?php get_search_form();?>
+				<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
+				<?php the_widget( 'WP_Widget_Archives' ); ?>
 
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
+			</div><!-- .page-content -->
+		</section><!-- .error-404 -->
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+	</main><!-- #main -->
 
 <?php
 get_footer();
